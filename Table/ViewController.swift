3799969d3ae34fb98cc2ddf6402510daabdd52fb
SeungHee Seo <https://github.com/blingsh>
2022-01-29 16:55:49 +0900

    테이블뷰만들기

--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -14,14 +14,18 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "myCell")
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
 
 }
